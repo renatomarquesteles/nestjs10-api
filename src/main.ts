6a8371965030b1760dbadec8e33dc464518863f9
Renato <renatomarquesteles@gmail.com>
2023-08-17 18:02:14 -0300

    adds data validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/prisma.exception-filter';
 import { CatchAllErrorsExceptionFilter } from './exceptions-filters/catch-all-errors.exception-filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,12 @@ async function bootstrap() {
   app.useGlobalFilters(
     new CatchAllErrorsExceptionFilter(httpAdapter),
     new PrismaExceptionFilter(),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422, // Unprocessable Content
+    }),
   );
 
   await app.listen(3000);
