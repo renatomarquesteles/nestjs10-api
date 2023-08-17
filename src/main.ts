7caf4a99cfab934e59baf91d8a2dac2a24131417
Renato <renatomarquesteles@gmail.com>
2023-08-17 17:37:32 -0300

    adds filter exception to catch all errors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/prisma.exception-filter';
+import { CatchAllErrorsExceptionFilter } from './exceptions-filters/catch-all-errors.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
 
-  app.useGlobalFilters(new PrismaExceptionFilter());
+  app.useGlobalFilters(
+    new CatchAllErrorsExceptionFilter(httpAdapter),
+    new PrismaExceptionFilter(),
+  );
 
   await app.listen(3000);
 }
